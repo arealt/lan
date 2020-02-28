ef2eb904b7b57e7258e5ef1f6292dc3703e373be
arealt <39209788+arealt@users.noreply.github.com>
2020-02-28 15:19:14 +0800

    Add files via upload

---
+++ precache-manifest.5f9204e3f5404c7fa1da0cfb92f3fa3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b87979f50894a8043515b85700ba22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e783c2eee311a40f8f7d",
+    "url": "/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "730afe392815987431f4",
+    "url": "/static/css/main.9798e0bd.chunk.css"
+  },
+  {
+    "revision": "e783c2eee311a40f8f7d",
+    "url": "/static/js/2.52a7d7ba.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.52a7d7ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730afe392815987431f4",
+    "url": "/static/js/main.5544cb1f.chunk.js"
+  },
+  {
+    "revision": "05e28521afc508de398d",
+    "url": "/static/js/runtime-main.061a52a8.js"
+  }
+]);
